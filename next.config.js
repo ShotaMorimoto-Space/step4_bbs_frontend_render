c08184f584d74c392442e7c725aca29cd01c846b
ShotaMorimoto-Space <s.morimoto1101@gmail.com>
2025-08-22 22:48:08 +0900

    5 commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { typedRoutes: true }
+  experimental: { typedRoutes: true },
+  poweredByHeader: false,
+  compress: true,
+  generateEtags: false
 }
 module.exports = nextConfig
